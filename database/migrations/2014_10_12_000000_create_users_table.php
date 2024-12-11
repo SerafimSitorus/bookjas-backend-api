e535fd3afe1585536a8990dd6fcd50f9471a495b
Serafim Edgar Pandamei Sitorus <serafim@gmail.com>
2024-12-11 09:56:29 +0700

    penambahan database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('nama');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('foto_profil')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('token');
             $table->timestamps();
+            $table->enum('status', ['User', 'Admin'])->default('User');
         });
     }
 
